638657781204606b9ce4442256113ee0be31a179
Alan Perez <alanipce@gmail.com>
2018-12-20 13:29:21 -0800

    Display modals in full screen
    * Adjust modal styles to fill viewport and reposition close button.
    * Improve MovieListing to only render details section when available
    * Apply whitespace to movie listing sections by default with optional modifier to remove padding.

--- src/MovieListing.js
+++ src/MovieListing.js
@@ -7,27 +7,29 @@ class MovieListing extends JSXComponent {
 
         return (
             <div class="movie-listing">
-                <section class="movie-listing__section">
+                <section class="movie-listing-section movie-listing-section--no-padding">
                     <div class="hero" style={{backgroundImage: `url(${movie.heroArtworkUrl})`}}>
                         <h2 class="hero__title">{movie.title}</h2>
                     </div>
                 </section>
-                <section class="movie-listing__section">
-                    {movie.details &&
-                        <div class="movie-overview">
-                            <div class="movie-overview__metadata">
-                                <div class="movie-overview__runtime"><MovieRuntime minutes={movie.details.runtime} /></div>
-                                <div class="movie-overview__genres">{movie.details.genres.join(", ")}</div>
-                            </div>
-                            <div class="movie-overview__intro">
-                                {movie.details.tagline && 
-                                    <div class="movie-overview__tagline">{movie.details.tagline}</div> 
-                                }
-                                <div class="movie-overview__summary">{movie.overview}</div>
+                {movie.details &&
+                    <section class="movie-listing-section">
+                        <div class="container">
+                            <div class="movie-overview">
+                                <div class="movie-overview__metadata">
+                                    <div class="movie-overview__runtime"><MovieRuntime minutes={movie.details.runtime} /></div>
+                                    <div class="movie-overview__genres">{movie.details.genres.join(", ")}</div>
+                                </div>
+                                <div class="movie-overview__intro">
+                                    {movie.details.tagline && 
+                                        <div class="movie-overview__tagline">{movie.details.tagline}</div> 
+                                    }
+                                    <div class="movie-overview__summary">{movie.overview}</div>
+                                </div>
                             </div>
                         </div>
-                    }
-                </section>
+                    </section>
+                }
             </div>
         );
     }
